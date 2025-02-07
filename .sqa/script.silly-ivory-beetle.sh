950d291fbf0bd42ecbdbb04c3343d019216a692b
Pablo Orviz <orviz@ifca.unican.es>
2025-02-07 15:36:15 +0100

    Add JePL file structure

---
+++ .sqa/script.silly-ivory-beetle.sh
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: Copyright contributors to the Software Quality Assurance as a Service (SQAaaS) project <sqaaas@ibergrid.eu>
+# SPDX-FileContributor: Pablo Orviz <orviz@ifca.unican.es>
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+(
+cd gitlab.inf.unibz.it/earth_observation_public/raster-to-stac &&
+    rst-lint --level warning --format json .
+)
